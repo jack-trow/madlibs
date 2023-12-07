120f848d1fd3c3325171b24a64b441a57b02414a
Jack Trowbridge <101287756+jack-trow@users.noreply.github.com>
2023-12-07 09:49:20 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+print ('Mad Lib')
+
+name1 = input("Say a name. ")
+
+place1 = input("Name a place. ")
+
+num1 = input("Name a number. ")
+
+adj1 = input("Name an adjective. ")
+
+adj2 = input("Name an adjective. ")
+
+verb1 = input("Name a verb ending in -ing. ")
+
+num2 = input("Name a number. ")
+
+nickname1 = input("Name an nickname. ")
+
+print("You and your friend, " + name1 + ", decided to go to " + place1 + ".")
+print("You both like " + place1 + ", but it is " + num1 + " miles away.")
+print("You both like how " + adj1 + " and " + adj2 + " it is. You want to go with")
+print("your friend, but you hate how " + verb1 + " they can be. If you had")
+print("to rate how " + verb1 + " he could be, it would be " + num2 + "/10 for sure.")
+print("You finally tell them, and come back by calling you a " + nickname1 + ".")
